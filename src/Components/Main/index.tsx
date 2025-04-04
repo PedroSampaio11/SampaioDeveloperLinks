fa14ebc4594e0a6133e782e90aa2567e965e6ceb
PedroSampaioDev <pedrosampaio11@icloud.com>
2025-04-04 19:14:13 -0300

    fix: corrge Method

--- src/Components/Main/index.tsx
+++ src/Components/Main/index.tsx
@@ -1,15 +1,15 @@
 import { MainContainer } from "./styles";
 
 import { VizionSection } from "../Vizion/VizionSection";
-import { MethodSection } from "../Method/MethodSection";
 import { IntroSection } from "../Intro/IntroSection";
+import { MethodSection } from "../Method/MethodSection";
 MethodSection;
 export const Main: React.FC = () => {
   return (
     <MainContainer>
       <IntroSection/>
       <VizionSection />
-      <MethodSection />
+      <MethodSection/>
     </MainContainer>
   );
 };
