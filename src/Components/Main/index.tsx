3901a76bfc0786ba8097ec6d12585222002bfd0c
PedroSampaioDev <pedrosampaio11@icloud.com>
2025-04-04 19:12:04 -0300

    fix: corrige case

--- src/Components/Main/index.tsx
+++ src/Components/Main/index.tsx
@@ -1,14 +1,14 @@
 import { MainContainer } from "./styles";
-import { IntroSection } from "../Intro/IntroSection";
+import { IntroSection } from "../Intro/introSection";
 import { VizionSection } from "../Vizion/VizionSection";
-import { MethodSection } from "../Method/methodSection";
-MethodSection
+import { MethodSection } from "../Method/MethodSection";
+MethodSection;
 export const Main: React.FC = () => {
   return (
     <MainContainer>
-      <IntroSection/>
-      <VizionSection/>
-      <MethodSection/>
+      <IntroSection />
+      <VizionSection />
+      <MethodSection />
     </MainContainer>
   );
 };
