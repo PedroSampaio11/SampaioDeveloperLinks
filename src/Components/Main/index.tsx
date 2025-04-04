22bf5ec73201236471a06e2ab7efdc7d3794d508
PedroSampaioDev <pedrosampaio11@icloud.com>
2025-04-04 19:14:40 -0300

    fix: corrige Vizion

--- src/Components/Main/index.tsx
+++ src/Components/Main/index.tsx
@@ -1,14 +1,13 @@
 import { MainContainer } from "./styles";
-
-import { VizionSection } from "../Vizion/VizionSection";
 import { IntroSection } from "../Intro/IntroSection";
 import { MethodSection } from "../Method/MethodSection";
+import { VizionSection } from "../Vizion/VizionSection";
 MethodSection;
 export const Main: React.FC = () => {
   return (
     <MainContainer>
       <IntroSection/>
-      <VizionSection />
+      <VizionSection/>
       <MethodSection/>
     </MainContainer>
   );
