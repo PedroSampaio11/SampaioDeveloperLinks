99b05a9f1aad75c8da6106b2a32d4c5c2ff0de84
PedroSampaioDev <pedrosampaio11@icloud.com>
2025-04-07 14:57:29 -0300

    feat : add webkits

--- src/styles/GlobalStyles.tsx
+++ src/styles/GlobalStyles.tsx
@@ -17,5 +17,26 @@ a {
 
 * {
     box-sizing: border-box;
+    &::-webkit-scrollbar {
+        width: 0.6rem;
+        height: 0.5rem;
+        background-color: ${({ theme }) => theme.colors.background};
+    }
+
+    &::-webkit-scrollbar-thumb {
+        border-radius: 5px;
+        background: ${({ theme }) => theme.colors.purple500};
+    }
+
+    ::-moz-selection {
+        background: ${({ theme }) => theme.colors.purple500};
+        color: #fff;
+        text-shadow: none;
+    }
+    &::selection {
+        background: ${({ theme }) => theme.colors.gray500};
+        color: #fff;
+        text-shadow: none;
+    }
 }
 `;
